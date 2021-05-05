edb7dcd0305d71e77e3f0730bb27ee85f6dc81a2
rem-my <ethandavidbarrow@outlook.com>
2021-05-05 07:58:04 +0100

    svelte

---
+++ src/routes/$layout.svelte
@@ -0,0 +1,50 @@
+<svelte:head>
+    <link href="/src/style/main.css" rel="stylesheet" />
+	<title>rem's site</title>
+
+    <meta http-equiv="Cache-control" content="no-cache">
+
+    <meta property="og:title" content="rem's site" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://rem-my.github.io" />
+    <meta property="og:image" content="https://rem-my.github.io/src/assets/siteLogo.png" />
+    <meta property="og:description" content="I'm remmy, but you can call me rem. And this is my site!" />
+    <meta name="theme-color" content="#FF7600">
+
+    <meta name="twitter:card" content="summary_large_image">
+
+</svelte:head>
+
+<body>
+    <div id="navbar" class="navbar" >
+        <a class="navButton" href="/">Home</a>
+        <a class="navButton" href="/src/main/credits/">Site credits</a>
+        <a class="imgButton" href="https://github.com/rem-my/" target="_blank" >
+            <img class="gh-logo" src="/src/assets/github.png" alt="GitHub" width="20" height="20">
+        </a>
+        <a class="imgButton" href="https://discord.com/users/257109471589957632" target="_blank" >
+            <img class="dc-logo" src="/src/assets/discord.png" alt="Discord" width="20" height="20">
+        </a>
+    </div>
+</body>
+
+<style>
+    .navbar {
+    background-color: #202020;
+    color: #fff;
+    padding: 10px;
+    }
+
+    .navButton {
+        padding: 5px;
+        border-radius: 8px;
+        background-color: #303030;
+        color: #fff;
+    }
+
+    .imgButton {
+        vertical-align: middle;
+        float: right;
+        margin-left: 5px;
+    }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<body>
+
+<!--Main Title-->
+<div id="title" class="title" >
+    <img src="/src/assets/siteLogo.png" width="128" alt="fox" height="128" >
+    <h1>Hello!</h1>
+    <p>I'm remmy, but you can call me rem. And this is my site!</p> 
+  </div>
+
+<!--Projects Title-->
+  <div id="projectsTitle" class="projectsTitle" >
+    <h2>Projects</h2>
+  </div>
+
+<!--Projects Section-->
+
+<!--ZMD Card-->
+  <div class="container">
+      <a href="//github.com/zmd" target="_blank">
+          <div class="item" style='background: url("/src/assets/zmd.png")'>
+              <div class="cardTitle">
+                  ZMD
+              </div>
+              <div class="spacer"></div>
+              <div class="description">
+                  A custom, open source command line for Windows in pure batch.
+              </div>
+          </div>
+      </a>
+  </div>
+</body>
