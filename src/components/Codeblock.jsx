aa641565ae0691e8a48f69d01df15f85dce61077
Beef <ethandavidbarrow@outlook.com>
2021-10-29 23:24:54 +0100

    Colour inconsistency

--- src/components/Codeblock.jsx
+++ src/components/Codeblock.jsx
@@ -3,7 +3,7 @@
 export default function({ children }) {
     return (
         <>
-        <code class="block p-6 mt-6 mb-3 whitespace-pre bg-light-600 dark:bg-dark-300 w-full overflow-hidden shadow-md <md:whitespace-normal">{children}</code>
+        <code class="block p-6 mt-6 mb-3 whitespace-pre bg-light-600 dark:bg-dark-100 w-full overflow-hidden shadow-md <md:whitespace-normal">{children}</code>
         <p class="text-xs mb-6 text-dark-50">Remind me to fix my codeblock component...</p>
         </>
     )
