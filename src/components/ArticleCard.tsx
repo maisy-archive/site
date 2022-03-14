d53923212661da01f9938f9fc9f375cd8e39a564
Beef <beefers@riseup.net>
2022-03-14 07:40:29 +0000

    [Performance] Pagespeed my beloved - hopefully last performance commit for a while

--- src/components/ArticleCard.tsx
+++ src/components/ArticleCard.tsx
@@ -16,7 +16,7 @@ export default ({ children, className, path, name, description, date, tags }: Ar
         <a href={path} className={`${className ? className : ""} mt-5 p-5 <lg:mx-auto w-full h-auto transition-all bg-light-tertiary shadow shadow-light-quarternary break-words dark:(bg-dark-secondary shadow-dark-secondary)`}>
                 <Header type="h5">{name}</Header>
                 <div class="bg-light-quarternary rounded-full container my-1 mx-auto w-max px-2 dark:(bg-dark-quarternary)">{tags.join(", ")}</div>
-                <img class="transition-all inline align-middle w-5 h-5 dark:(filter invert) mr-1" src={DateIcon}/><span class="text-sm">{date.toDateString()}</span>
+                <img width="20px" height="20px" class="transition-all inline align-middle w-5 h-5 dark:(filter invert) mr-1" src={DateIcon}/><span class="text-sm">{date.toDateString()}</span>
                 <p className="mt-5">{description}</p>
         </a>
     );
