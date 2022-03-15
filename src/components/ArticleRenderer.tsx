604a404ee8461231efcd657ff9edde4ab0ce4d18
Beef <beefers@riseup.net>
2022-03-15 20:43:42 +0000

    [Components > ArticleRenderer] Never will I be happy with this component

--- src/components/ArticleRenderer.tsx
+++ src/components/ArticleRenderer.tsx
@@ -19,21 +19,21 @@ export default ({ src }: ArticleRendererProps) => {
                 .solidown-markdown-root h1 {
                     font-family: "Poppins";
                     margin-top: unset;
+                    margin-bottom: 1rem;
                     font-size: 2.25rem;
                 }
                 .solidown-markdown-root h2 {
                     font-family: "Poppins";
+                    margin-bottom: 0.25rem;
                     margin-top: 1.2rem;
                     font-size: 1.5rem;
                 }
                 .solidown-markdown-root h3 {
                     font-family: "Poppins";
+                    margin-bottom: 0.25rem;
                     margin-top: 1rem;
                     font-weight: normal;
                     font-size: 1.25rem;
-                }
-                .solidown-markdown-root p {
-                    margin-bottom: 0.5rem;
                 }
                 .solidown-markdown-root a {
                     color: #09bc8a;
