727e90738991e4c3245999c176372ea66e7ebfe0
Beef <beefers@riseup.net>
2022-03-24 07:32:15 +0000

    [Compatibility] Fix site on older Safarigit add .git add .

--- src/components/CreditsUser.tsx
+++ src/components/CreditsUser.tsx
@@ -2,11 +2,10 @@ interface CreditsUserProps {
     children?: string;
     className?: string;
     username: string;
-    src: string;
 }
 
-export default ({ children, className, username, src }: CreditsUserProps) => {
+export default ({ children, className, username }: CreditsUserProps) => {
     return (
-        <a className="text-accent underline" href={`//github.com/${username}/`}><img className="inline-block align-middle bg-muted rounded-full bg-opacity-25 mr-2" src={src} width="50" height="50" />{username}</a>
+        <a className="text-accent underline" href={`//github.com/${username}/`}><img className="inline-block align-middle bg-muted rounded-full bg-opacity-25 mr-2" src={`//github.com/${username}.png`} width="50" height="50" />{username}</a>
     );
 };
--- src/routes/Credits.tsx
+++ src/routes/Credits.tsx
@@ -3,9 +3,6 @@ import Header from "../components/Header";
 import CreditsIcon from "@material-design-icons/svg/round/engineering.svg";
 import CreditsUser from "../components/CreditsUser";
 
-const beefAvatar: string = (await (await fetch("https://api.github.com/users/Beefers")).json()).avatar_url;
-const sinkAvatar: string = (await (await fetch("https://api.github.com/users/Yellowsink")).json()).avatar_url;
-
 export default {
     name: "Credits",
     path: "/credits",
@@ -15,8 +12,8 @@ export default {
             <>
                 <div className="container mx-auto mt-10 text-center">
                     <Header type="h1" className="mb-2">Site credits</Header>
-                    <p className="text-xl mb-2"><CreditsUser username="Beefers" src={beefAvatar}/> - Site design and programming</p>
-                    <p className="text-xl mb-2"><CreditsUser username="Yellowsink" src={sinkAvatar}/> - Inspiration and indirect assistance</p>
+                    <p className="text-xl mb-2"><CreditsUser username="Beefers"/> - Site design and programming</p>
+                    <p className="text-xl mb-2"><CreditsUser username="Yellowsink"/> - Inspiration and indirect assistance</p>
                 </div>
             </>
         );
